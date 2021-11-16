acb6b213d36abc8ee917b88fa10a8fd0ccdb95b8
Junior Souza <fabriciojuniorsc@gmail.com>
2021-11-15 21:48:39 -0300

    Acompanhar eventos

--- src/app/acompanhar-eventos/acompanhar-eventos.component.ts
+++ src/app/acompanhar-eventos/acompanhar-eventos.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { BASE_URL } from '../app.component';
+import { Eventos } from '../models/eventos';
+import { EventosService } from '../services/eventos.service';
 import { ToastMessageComponent } from '../shared/toast-message/toast-message.component';
 
 @Component({
@@ -9,9 +12,39 @@ import { ToastMessageComponent } from '../shared/toast-message/toast-message.com
 export class AcompanharEventosComponent implements OnInit {
 
   @ViewChild('toast') private toast: ToastMessageComponent;
-  constructor() { }
+  public proximo: boolean = false;
+  paginasTotais: number;
+  pagina: number;
+  eventos: Eventos[] = [];
+  showLoading = false;
+
+  URL_RELATORIO = BASE_URL + "/relatorio/eventos";
+
+  constructor(private eventosService : EventosService) { }
 
   ngOnInit(): void {
+    this.getByPage(1);
+  }
+
+  getByPage(pagina) {
+    if (!this.proximo && pagina > this.pagina) {
+      return;
+    }
+    this.showLoading = true;
+    this.eventosService.getPage(pagina).toPromise()
+      .then(res => {
+        this.eventos = res.content;
+        this.proximo = !res.last;
+        this.paginasTotais = res.totalPages;
+        this.pagina = res.number + 1;
+      })
+      .catch(err => {
+        console.log(err);
+
+      })
+      .finally(() => {
+        this.showLoading = false;
+      })
   }
 
 }
